46366ad750c7ee95e062853a6cc9d8b27fa4a606
koorosh safeashrafi <k.seif@kargosha.com>
2022-11-02 13:49:36 +0330

    we can set data on throws

--- src/Exceptions/Exception.php
+++ src/Exceptions/Exception.php
@@ -7,9 +7,10 @@ class Exception
     public function render(\Throwable $e)
     {
         $errors = $this->getErrors($e);
+        $data = $this->getData($e);
         $code = $this->getStatusCode($e);
 
-        return $this->setBody(e: $e, errors: $errors, code: $code);
+        return $this->setBody(e: $e, errors: $errors, code: $code, data: $data);
     }
 
     public function getErrors(\Throwable $e)
@@ -17,6 +18,11 @@ class Exception
         return match ($e::class) {
             default => $this->default($e),
         };
+    }
+
+    public function getData(\Throwable $e)
+    {
+        return method_exists($e, 'getData') ? $e->getData() : null;
     }
 
     private function default(\Throwable $e)
@@ -35,18 +41,18 @@ class Exception
         return $code;
     }
 
-    private function setBody(\Throwable $e, mixed $errors, int $code)
+    private function setBody(\Throwable $e, mixed $errors, int $code, mixed $data)
     {
         response()->code($code);
 
         if (response()->getHttpRequest()->isJson()) {
-            $body = response()->json(null, $errors, $code);
+            $body = response()->json($data, $errors, $code);
         } else {
             $file = _APP_PATH_VIEW_ . '/exceptions/' . $code . '.php';
             if (file_exists($file)) {
                 $body = response()->tpl('exceptions/' . $code, ['e' => $e]);
             } else {
-                $body = response()->json(null, $errors, $code);
+                $body = response()->json($data, $errors, $code);
             }
         }
 
--- src/Exceptions/HttpException.php
+++ src/Exceptions/HttpException.php
@@ -4,4 +4,16 @@ namespace One\Exceptions;
 
 class HttpException extends \Exception
 {
+	private ?array $data;
+
+	public function __construct(string $message="", int $code=0, array $data=null, ?\Throwable $previous=null)
+	{
+		$this->data = $data;
+		parent::__construct(message: $message, code: $code, previous: $previous);
+	}
+
+	public function getData()
+	{
+		return $this->data;
+	}
 }
